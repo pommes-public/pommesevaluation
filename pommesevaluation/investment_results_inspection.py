7fb6a65f3c26d1b5a219527dfe91ed65762ec88a
Johannes Kochems <jokochems@web.de>
2022-09-21 16:30:48 +0200

    Add first draft for investment results inspection

---
+++ pommesevaluation/investment_results_inspection.py
@@ -0,0 +1,174 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def preprocess_raw_results(results_raw):
+    """Preprocess raw investment results
+
+    Parameters
+    ----------
+    results_raw : pd.DataFrame
+        Raw investment results
+
+    Returns
+    -------
+    processed_results : pd.DataFrame
+        Preprocessed investment results
+    """
+    processed_results = results_raw.copy()
+    processed_results.index = processed_results.index.str.split(expand=True)
+    processed_results.index.names = ["from", "to", "year"]
+    processed_results.reset_index(inplace=True)
+    processed_results["from"] = processed_results["from"].str.strip("(',")
+    processed_results["to"] = processed_results["to"].str.strip(")',")
+    processed_results["year"] = processed_results["year"].str.strip(")")
+
+    # Adjust storage labels
+    processed_results.loc[
+        (processed_results["from"].str.contains("storage"))
+        & (processed_results["to"].str.contains("None")),
+        "from",
+    ] = (
+        processed_results["from"] + "_capacity"
+    )
+
+    processed_results.loc[
+        (processed_results["from"].str.contains("storage"))
+        & (processed_results["to"].str.contains("DE_bus_el")),
+        "from",
+    ] = (
+        processed_results["from"] + "_inflow"
+    )
+
+    processed_results.loc[
+        (processed_results["from"].str.contains("DE_bus_el"))
+        & (processed_results["to"].str.contains("storage")),
+        "from",
+    ] = (
+        processed_results["to"] + "_outflow"
+    )
+
+    processed_results = processed_results.rename(
+        columns={"from": "unit"}
+    ).drop(columns="to")
+    string_to_drop = ["DE_storage_el_", "DE_transformer_", "_new_built"]
+    for string in string_to_drop:
+        processed_results["unit"] = processed_results["unit"].str.replace(
+            string, ""
+        )
+
+    return processed_results
+
+
+def aggregate_investment_decision_results(
+    processed_results, energy_carriers, by="energy_carrier"
+):
+    """Aggregate preprocesed investment results by energy carrier or technology
+
+    Parameters
+    ----------
+    processed_results : pd.DataFrame
+        Preprocessed investment results
+
+    energy_carriers : list
+        List of feasible energy carriers
+
+    by : str
+        Rule to group by; either `energy_carrier` or `technologies`
+
+    Returns
+    -------
+    aggregated_results : pd.DataFrame
+        Aggregated results incl. investments in storage inflow
+
+    other_storages_results : pd.DataFrame
+        Results for investments in storage capacity and storage outflow
+    """
+    aggregated_results = processed_results.copy()
+    aggregated_results[["fuel", "tech"]] = aggregated_results[
+        "unit"
+    ].str.split("_", 1, expand=True)
+
+    storage_technologies = ["PHS", "battery"]
+    storage_elements = ["_capacity", "_outflow"]
+    storages = [a + b for a in storage_technologies for b in storage_elements]
+
+    energy_carriers = energy_carriers
+    technologies = r"GT|ST|CC|FC"
+
+    grouping_col = by
+    if by == "energy_carrier":
+        aggregated_results["energy_carrier"] = np.where(
+            aggregated_results["fuel"].isin(energy_carriers),
+            aggregated_results["fuel"],
+            aggregated_results["unit"],
+        )
+
+    elif by == "technology":
+        aggregated_results["technology"] = np.where(
+            aggregated_results["tech"].str.contains(technologies),
+            aggregated_results["tech"].str.split("_", expand=True).iloc[:, 0],
+            aggregated_results["unit"],
+        )
+    else:
+        raise ValueError(
+            f"Aggregation mode {by} not defined; "
+            f"must be `energy_carrier` or `technologies`."
+        )
+
+    aggregated_results = aggregated_results.groupby(
+        [grouping_col, "year"]
+    ).sum()
+
+    other_storages_results = aggregated_results.loc[[idx for idx in storages]]
+    aggregated_results.drop(index=other_storages_results.index, inplace=True)
+
+    return aggregated_results, other_storages_results
+
+
+def plot_single_investment_variable(
+        results, variable_name, colors=None, aggregation="energy_carrier"
+):
+    """Plot a single investment-related variable from results data set
+
+    Parameters
+    ----------
+    results : pd.DataFrame
+        Data set containing all investment results
+
+    variable_name : str
+        Particular variable to plot;
+        one of ['invest', 'old', 'old_end', 'old_exo', 'total']
+
+    colors : dict or None
+        Colors to use if given
+
+    aggregation : str or None
+        Determines which kind of aggregation has been made;
+        aggregation by `energy_carrier` or by `technology`
+    """
+    ylabels = {
+        "invest": "newly invested capacity",
+        "old": "total decommissioned capacity",
+        "old_end": "capacity decommissioned because of lifetime",
+        "old_exo": "capacity decommissioned because after initial age",
+        "total": "total installed capacity",
+    }
+
+    plot_data = results[[variable_name]].reset_index()
+    plot_data = plot_data.pivot(
+        index=aggregation, columns="year", values=variable_name
+    )
+    fig, ax = plt.subplots(figsize=(12, 5))
+    if colors:
+        plot_data = plot_data.loc[[col for col in colors]]
+        _ = plot_data.T.plot(kind="bar", stacked=True, ax=ax, color=colors)
+    else:
+        _ = plot_data.T.plot(kind="bar", stacked=True, ax=ax)
+    _ = plt.legend(bbox_to_anchor=[1.02, 1.02])
+    _ = plt.xlabel("year")
+    _ = plt.ylabel(f"{ylabels[variable_name]} in MW")
+    _ = plt.tight_layout()
+
+    _ = plt.show()
