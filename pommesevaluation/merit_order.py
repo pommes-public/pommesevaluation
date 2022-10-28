4a78b759edd6b1670885b7ec2a913deb7ff5dd61
Johannes Kochems <jokochems@web.de>
2022-10-28 18:58:57 +0200

    Generalize merit order plot

--- pommesevaluation/merit_order.py
+++ pommesevaluation/merit_order.py
@@ -3,6 +3,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def prepare_market_values_from_results(costs_market_values, simulation_year):
+    """Prepare market values obtained from results file (reindexing)
+
+    Parameters
+    ----------
+    costs_market_values : pd.DataFrame
+        Market values obtained from results, one value per month, int indexed
+
+    simulation_year : int
+        Year simulated
+
+    Returns
+    -------
+    costs_market_values : pd.DataFrame
+        Reshaped version of data
+    """
+    costs_market_values.index = [
+        f"{simulation_year}-0{el}-01 00:00:00"
+        if len(str(el)) == 1
+        else f"{simulation_year}-{el}-01 00:00:00"
+        for el in costs_market_values.index
+    ]
+    costs_market_values.loc[
+        f"{simulation_year + 1}-01-01 00:00:00"
+    ] = costs_market_values.iloc[-1]
+    costs_market_values.index = pd.to_datetime(costs_market_values.index)
+    costs_market_values = costs_market_values.resample("H").ffill()
+
+    return costs_market_values
+
+
 def reshape_merit_order_data(blocks):
     """Reshape merit order data to facilitate plotting
 
@@ -43,14 +74,25 @@ def reshape_merit_order_data(blocks):
 
     # Create a DataFrame out of array
     merit_order = pd.DataFrame(
-        data=merit_order, columns=["fuel", "costs_marginal", "capacity_cumulated"]
+        data=merit_order,
+        columns=["fuel", "costs_marginal", "capacity_cumulated"],
     )
     return merit_order.astype(
         {"capacity_cumulated": "float32", "costs_marginal": "float32"}
     )
 
 
-def plot_merit_order(merit_order, colors, ylim=(-100, 200), set_ylim=True):
+def plot_merit_order(
+    merit_order,
+    colors,
+    ylim=(-100, 200),
+    set_ylim=True,
+    interval=20,
+    height=5,
+    save=True,
+    path_plots="./plots/",
+    file_name="merit_order",
+):
     """Plot a merit order based on given data
 
     Parameters
@@ -67,8 +109,23 @@ def plot_merit_order(merit_order, colors, ylim=(-100, 200), set_ylim=True):
     set_ylim : boolean
         If False, use minima and maxima of data set plus margins
         to derive ylim
+
+    interval : int
+        interval to use for horizontal grid lines
+
+    height : int
+        height of plot
+
+    save : boolean
+        If True, save to disk
+
+    path_plots : str
+        Path to store the plot
+
+    file_name : str
+        file name for the plot
     """
-    fig, ax = plt.subplots(figsize=(12, 5))
+    fig, ax = plt.subplots(figsize=(12, height))
     for fuel in merit_order["fuel"].unique():
         _ = ax.fill_between(
             x="capacity_cumulated",
@@ -94,11 +151,14 @@ def plot_merit_order(merit_order, colors, ylim=(-100, 200), set_ylim=True):
             ),
         )
     _ = ax.grid(axis="y")
-    _ = plt.yticks(np.arange(-100, 121, 10.0))
-    _ = plt.xlabel("Kumulierte Leistung [MW]")
-    _ = plt.ylabel("Grenzkosten [€/MWh]")
+    _ = plt.yticks(np.arange(ylim[0], ylim[1] + 1, interval))
+    _ = plt.xlabel("Cumulated power [MW]")
+    _ = plt.ylabel("marginal costs resp. opportunity costs [€/MWh]")
     _ = ax.legend(loc="lower right", ncol=4)
 
     plt.tight_layout()
+
+    if save:
+        _ = plt.savefig(f"{path_plots}{file_name}.png", dpi=300)
 
     plt.show()
