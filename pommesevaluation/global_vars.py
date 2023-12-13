329e7a9cd0610bd79aa80625fdae655b43d52d3e
Johannes Kochems <jokochems@web.de>
2023-12-13 18:19:02 +0100

    Maintain global variables centrally

---
+++ pommesevaluation/global_vars.py
@@ -0,0 +1,39 @@
+FUELS = {
+    "biomass": "#15b01a",
+    "uranium": "#e50000",
+    "lignite": "#7f2b0a",
+    "hardcoal": "#000000",
+    "natgas": "#ffd966",
+    "hydrogen": "#6fa8dc",
+    "mixedfuels": "#a57e52",
+    "otherfossil": "#d8dcd6",
+    "waste": "#c04e01",
+    "oil": "#aaa662",
+}
+
+FUELS_RENAMED = {
+    "German": {
+        "biomass": "Biomasse",
+        "uranium": "Kernenergie",
+        "lignite": "Braunkohle",
+        "hardcoal": "Steinkohle",
+        "natgas": "Erdgas",
+        "hydrogen": "Wasserstoff",
+        "mixedfuels": "gemischte Brennstoffe",
+        "otherfossil": "sonstige Konventionelle",
+        "waste": "Abfall",
+        "oil": "Erdöl",
+    },
+    "English": {
+        "biomass": "biomass",
+        "uranium": "uranium",
+        "lignite": "lignite",
+        "hardcoal": "hard coal",
+        "natgas": "natural gas",
+        "hydrogen": "hydrogen",
+        "mixedfuels": "mixed conventionals",
+        "otherfossil": "other conventionals",
+        "waste": "waste",
+        "oil": "oil",
+    },
+}
